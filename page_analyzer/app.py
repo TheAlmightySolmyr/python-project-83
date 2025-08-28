3080196c2c442fed5e12be13ac1de17fb4bde2ae
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-08-28 19:26:04 +0300

    testing

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,7 @@
 import os
 
-import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, render_template
-
-load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL)
 
 load_dotenv()
 app = Flask(__name__)
