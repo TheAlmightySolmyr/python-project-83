1660f502870c06e1991722683778b847022c6b6a
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-08-29 16:22:31 +0300

    upd. Func for db connection did not work

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,42 +1,36 @@
 import os
-import psycopg2
-import datetime
 
-from validators import url as validate
+import psycopg2
+from datetime import datetime
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, flash, redirect, render_template, request
+from validators import url as validate
 
 load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+conn.autocommit = True
 
-def get_db_connection():
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        conn.autocommit = True
-        return conn
-    except psycopg2.Error as e:
-        print(f"Ошибка подключения к базе данных: {e}")
-        return None
-    
-connect = get_db_connection()
 
 @app.route('/')
 def get_start_page():
     return render_template('start_page.html')
 
+
 @app.route('/urls')
 def get_url():
     return render_template('urls.html')
+
 
 @app.post('/')
 def post_url():
     formed = request.form.to_dict()
     url = formed.get('url')
     if validate(url):
-        with connect.cursor() as curs:
-            curs.execute('INSERT INTO urls(name, created_at) VALUES (%s, %s);', (url, datetime.datetime.now()))
+        with conn.cursor() as curs:
+            curs.execute('INSERT INTO urls(name, created_at) VALUES (%s, %s);', (url, datetime.now()))
             flash('Your url was successfully added', 'success')
     else:
         flash('url has mistakes', 'error')
