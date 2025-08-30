08dc5fe53b17a3df03c7f5e4359faf956c4d2187
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-08-30 22:27:41 +0300

    updated app.py, templates and db. Its finally connects

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 conn = psycopg2.connect(DATABASE_URL)
 conn.autocommit = True
+MAX_URL_LENGTH = 255
 
 
 @app.route('/')
@@ -24,15 +25,25 @@ def get_urls_page():
     return render_template('urls.html')
 
 
-@app.post('/')
+@app.post('/urls')
 def post_url():
-    formed = request.form.to_dict()
-    url = formed.get('url')
-    if validate(url):
-        with conn.cursor() as curs:
-            curs.execute('INSERT INTO urls(name, created_at) VALUES (%s, %s);', 
-                         (url, datetime.now()))
-            flash('Your url was successfully added', 'success')
-    else:
-        flash('url has mistakes', 'error')
+    url = request.form.get('url')
+    
+    if not validate(url) or len(url) >= MAX_URL_LENGTH:
+        flash('URL has mistakes', 'error')
         return redirect('/')
+    
+    with conn.cursor() as curs:
+        curs.execute('SELECT id FROM urls WHERE name = %s;', (url,))
+        existing_url = curs.fetchone()
+        
+        if existing_url:
+            flash('URL already exists in database', 'info')
+        else:
+            curs.execute('''
+                        INSERT INTO urls(name, created_at) 
+                        VALUES (%s, %s);
+                        ''', (url, datetime.now()))
+            flash('URL was successfully added', 'success')
+    
+    return redirect('/urls')
