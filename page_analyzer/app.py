750db2e3bf2f22dd35a9ad88fc5aef24643dd88b
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-09-08 21:47:37 +0300

    little rollback

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,11 @@
 import os
 from datetime import datetime
 
-from psycopg2.pool import SimpleConnectionPool
-import psycopg2
 import requests as req
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, url_for
+from psycopg2.pool import SimpleConnectionPool
 from validators import url as validate
 
 load_dotenv()
@@ -15,6 +14,39 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 pool = SimpleConnectionPool(1, 1, DATABASE_URL)  
 MAX_URL_LENGTH = 255
+
+
+def is_available(link):
+    try:
+        url = req.get(link)
+        url.raise_for_status()
+        return url.status_code
+    except req.RequestException:
+        return None
+    
+
+def get_h1(url):
+    html = req.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    h1 = soup.find('h1')
+    if h1:
+        return h1.get_text()
+
+
+def get_title(url):
+    html = req.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    title = soup.find('title')
+    if title:
+        return title.get_text()
+    
+
+def get_content(url):
+    html = req.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    content = soup.find('meta', attrs={'name': 'description'})
+    if content:
+        return content.get('content')
 
 
 @app.route('/')
@@ -54,7 +86,7 @@ def post_url():
     if not validate(url) or len(url) >= MAX_URL_LENGTH:
         flash('Некорректный URL', 'error')
         pool.putconn(conn)
-        return redirect('/')
+        return render_template('start_page.html'), 422
     
     with conn.cursor() as curs:
         curs.execute('SELECT id FROM urls WHERE name = %s;', (url,))
@@ -62,15 +94,17 @@ def post_url():
         
         if existing_url:
             flash('Страница уже существует', 'info')
+            url_id = existing_url[0]
         else:
             curs.execute('''
                         INSERT INTO urls(name, created_at) 
-                        VALUES (%s, %s);
+                        VALUES (%s, %s) RETURNING id;
                         ''', (url, datetime.now()))
+            url_id = curs.fetchone()[0]
             flash('Страница успешно добавлена', 'success')
         conn.commit()
     pool.putconn(conn)    
-    return redirect('/urls')
+    return redirect(url_for('get_url_page', id=url_id))
 
 
 @app.route('/urls/<int:id>')
@@ -83,7 +117,7 @@ def get_url_page(id):
         if not url:
             flash('Сайт не найден', 'error')
             pool.putconn(conn)
-            return redirect('/urls')
+            return render_template('urls.html'), 422
         
         curs.execute('''
             SELECT * FROM url_checks 
@@ -107,60 +141,37 @@ def check_url(id):
         if not row:
             flash('Сайт не найден', 'error')
             pool.putconn(conn)
-            return redirect('/urls')
+            return render_template('urls.html'), 422
         url = row[0]
     pool.putconn(conn)
     status_code = is_available(url)
-
-    if status_code:
-        h1 = get_h1(url)
-        title = get_title(url)
-        content = get_content(url)
-        conn = pool.getconn()
-        with conn.cursor() as curs:
-            curs.execute('''
-            INSERT INTO 
-            url_checks (url_id, status_code, h1, title, description, created_at)
-            VALUES (%s, %s, %s, %s, %s, %s);
-            ''', (id, status_code, h1, title, content, datetime.now()))
-            flash('Страница успешно проверена', 'success')
-            conn.commit()
-        pool.putconn(conn)
-
-    else:
-        pass
-    return redirect(url_for('get_url_page', id=id))
-
-
-def is_available(link):
+    conn = pool.getconn()
     try:
-        url = req.get(link)
-        url.raise_for_status()
-        return url.status_code
-    except req.RequestException:
-        flash('Произошла ошибка при проверке')
-        return None
+        with conn.cursor() as curs:
+            if status_code:
+                h1 = get_h1(url)
+                title = get_title(url)
+                content = get_content(url)
+                curs.execute('''
+                INSERT INTO 
+                url_checks 
+                    (url_id, status_code, h1, title, description, created_at)
+                VALUES (%s, %s, %s, %s, %s, %s);
+                ''', (id, status_code, h1, title, content, datetime.now()))
+                flash('Страница успешно проверена', 'success')
+            else:
+                curs.execute('''
+                    INSERT INTO url_checks (url_id, status_code, created_at)
+                    VALUES (%s, %s, %s);
+                ''', (id, 0, datetime.now()))
+                flash('Произошла ошибка при проверке', 'danger')
+            
+            conn.commit()
+            
+    except Exception:
+        conn.rollback()
+        flash('Произошла ошибка при сохранении проверки', 'danger')
+    finally:
+        pool.putconn(conn)
     
-
-def get_h1(url):
-    html = req.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    h1 = soup.find('h1')
-    if h1:
-        return h1.get_text()
-
-
-def get_title(url):
-    html = req.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.find('title')
-    if title:
-        return title.get_text()
-    
-
-def get_content(url):
-    html = req.get(url).text
-    soup = BeautifulSoup(html, 'html.parser')
-    content = soup.find('meta', attrs={'name': 'description'})
-    if content:
-        return content.get('content')
+    return redirect(url_for('get_url_page', id=id))
